232d5ecde44e56378fefccaab8a8a4bf844247e1
Ujwal Garg <54837462+Ujwalgarg2387@users.noreply.github.com>
2024-06-05 21:38:37 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,6 +5,8 @@ import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   //here paste the firebase code you will get it in the starting when you first create a project in firebase console
+  //got to firebase cosole by sigining in
+  //create a project/ register an app inside it(there will be option) / you will get the firebase code to add below
   apiKey: "YOUR_API_KEY",
   authDomain: "PROJECT_ID.firebaseapp.com",
   projectId: "PROJECT_ID", //project id like chat-21c80
