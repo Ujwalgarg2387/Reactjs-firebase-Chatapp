dca6c537511f8e657b9a3842f8d176b359351715
Ujwal Garg <54837462+Ujwalgarg2387@users.noreply.github.com>
2024-06-05 21:39:49 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -7,12 +7,12 @@ const firebaseConfig = {
   //here paste the firebase code you will get it in the starting when you first create a project in firebase console
   //got to firebase cosole by sigining in
   //create a project/ register an app inside it(there will be option) / you will get the firebase code to add below
-  apiKey: "YOUR_API_KEY",
-  authDomain: "PROJECT_ID.firebaseapp.com",
-  projectId: "PROJECT_ID", //project id like chat-21c80
-  storageBucket: "PROJECT_ID.appspot.com",
-  messagingSenderId: "SOME ID",
-  appId: "SOME ID"
+  apiKey: "AIzaSyAytch-71RVN1olRDHivmpqpY0nLFJbLdI",
+  authDomain: "chat-21c80.firebaseapp.com",
+  projectId: "chat-21c80",
+  storageBucket: "chat-21c80.appspot.com",
+  messagingSenderId: "707381422053",
+  appId: "1:707381422053:web:f094e2c4251b120f1b310c"
 };
 
 export const app = initializeApp(firebaseConfig);
